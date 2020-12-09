adfdd3513f16856ca8fcc30497d1362a36c6f1d1
Charlie B <>
2020-12-09 13:49:14 +0000

    add docstrings

--- rick_and_morty/api.py
+++ rick_and_morty/api.py
@@ -4,9 +4,17 @@ class RickandMorty:
     HOST = "https://rickandmortyapi.com/api"
     DOCS = "https://rickandmortyapi.com/documentation/"
     
-    def get_characters_results(self, page_num=1)):
+    def get_character_results(self, page_num=1):
         """
-        each page will return a maximum of 20 results
+        Get 20 characters from the specified page number
+        
+        Parameters
+        ----------
+        page_num = int: page to return
+        
+        Returns
+        -------
+        json: 20 characters with various fields
         """
         endpoint = "/character"
         params = {
@@ -15,23 +23,53 @@ class RickandMorty:
         data = requests.get(self.HOST + endpoint, params).json()
         return data['results']
 
-    def get_character_info(self:
+    def get_character_info(self):
         """
-        summary data of number of pages and characters available
+        Get summary of number of pages and characters available
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        json: information from rick and morty api
         """
         endpoint = "/character"
         data = requests.get(self.HOST + endpoint, params).json()
         return data['info']
     
     def get_character_single(self, id):
+        """
+        Get a character from their ID
+        
+        Parameters
+        ----------
+        id = int: id of the character to be returned
+        
+        Returns
+        -------
+        json: information about the character
+        """
         endpoint = "/character"
         params = {
-            "id": id
+            "id": int(id)
         }
         data = requests.get(self.HOST + endpoint, params).json()
         return data['results']
     
     def get_character_all(self):
+        """
+        Get a list of all the characters from the api
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        results = list: dictionaries of characters
+        """
         results = []
         num_of_pages = self.get_character_info()['info']['pages'] + 1
 
@@ -42,18 +80,52 @@ class RickandMorty:
             
         return results
     
-    def character_multi(self, ls):
+    def get_character_multi(self, ls):
+        """
+        Get mutliple characters using their id
+        
+        Parameters
+        ----------
+        ls = list: of integer ids
+        
+        Returns
+        -------
+        results = list: character dictionaries for ids
+        """
         results = []
         for i in ls:
             char = self.get_character_single(i)
             results.append(char['results'])
         return results
     
-    def character_filter(self, name, status, species, type, gender):
-        pass
-    
-    def character_count(self):
-        pass
+    def character_filter(self, name=None, status=None, species=None, type=None, gender=None):
+        """
+        Get characters that match criteria of 1 or more filters
+        
+        Parameters
+        ----------
+        name = str: name of character
+        status = str: character status (dead/alive/unknown)
+        species = str: species of character
+        type = str: type of sub-species
+        gender = str: male/female/genderless/unknown
+        
+        Returns
+        -------
+        data = json: characters matching criteria 
+        """
+        endpoint = '/character'
+        all_params = {
+            "name": name,
+            "status": status,
+            "species": species,
+            "type": type,
+            "gender": gender
+        }
+        params = {k:v for k,v in all_params.items() if v != None}
+        data = requests.get(self.HOST + endpoint, params).json()
+        
+        return data
     
     def episodes(self):
         endpoint="/episode"
