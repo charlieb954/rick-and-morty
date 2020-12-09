37544d9b5712ceeab1f0b427e82982ab11df9661
Charlie B <>
2020-12-09 12:39:18 +0000

    add info function to class

--- rick_and_morty/api.py
+++ rick_and_morty/api.py
@@ -4,17 +4,24 @@ class RickandMorty:
     HOST = "https://rickandmortyapi.com/api"
     DOCS = "https://rickandmortyapi.com/documentation/"
     
-    def get_character_info(self, page_num=1):
+    def get_characters_results(self, page_num=1)):
         """
         each page will return a maximum of 20 results
         """
-        
         endpoint = "/character"
         params = {
             "page": page_num
             }
         data = requests.get(self.HOST + endpoint, params).json()
-        return data
+        return data['results']
+
+    def get_character_info(self:
+        """
+        summary data of number of pages and characters available
+        """
+        endpoint = "/character"
+        data = requests.get(self.HOST + endpoint, params).json()
+        return data['info']
     
     def get_character_single(self, id):
         endpoint = "/character"
@@ -25,36 +32,28 @@ class RickandMorty:
         return data['results']
     
     def get_character_all(self):
+        results = []
         num_of_pages = self.get_character_info()['info']['pages'] + 1
-        
-        all_results = []
+
         for i in range(1, num_of_pages):
-            chars = self.get_character_info(i)
+            chars = self.get_character_results(i)
             for char in chars['results']:
-                all_results.append(char)
+                results.append(char)
             
-        return all_results
+        return results
     
     def character_multi(self, ls):
-        pass
+        results = []
+        for i in ls:
+            char = self.get_character_single(i)
+            results.append(char['results'])
+        return results
     
     def character_filter(self, name, status, species, type, gender):
         pass
     
     def character_count(self):
         pass
-    
-    def characters_results(self):
-        """
-        each page will return a maximum of 20 results
-        """
-        
-        endpoint = "/character"
-        params = {
-            "page": page_num
-            }
-        data = requests.get(self.HOST + endpoint, params).json()
-        return data['results']
     
     def episodes(self):
         endpoint="/episode"
