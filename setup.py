89241b467c574d869068c98aa6c00f4bb12bd7cb
Charlie <charlie@Charlies-MBP.broadband>
2023-05-17 20:28:41 +0100

    add requirements to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = open('requirements.txt').readlines()
 
 test_requirements = ['pytest>=3', ]
 
