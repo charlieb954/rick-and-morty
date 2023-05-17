d17ad3411e62847e773906791634c277ff347ba1
Charlie <charlie@Charlies-MBP.broadband>
2023-05-17 20:17:44 +0100

    remove unused import

--- tests/test_rickandmorty.py
+++ tests/test_rickandmorty.py
@@ -2,8 +2,6 @@
 
 """Tests for `rickandmorty` package."""
 
-import pytest
-
 
 from rickandmorty import Characters, Episodes, Locations
 
@@ -12,13 +10,16 @@ characters = Characters()
 episodes = Episodes()
 locations = Locations()
 
+
 def test_get_character_single():
     character = characters.get_character_single(id=826)
     assert character['name'] == 'Butter Robot'
 
+
 def test_get_episode_single():
     episode = episodes.get_episode_single(3)
     assert episode['name'] == 'Anatomy Park'
+
 
 def test_get_location_single():
     location = locations.get_location_single(id=1)
