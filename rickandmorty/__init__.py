b56c88476a0d238af584027aa59f487b51f00173
Charlie <charlie@Charlies-MBP.broadband>
2023-06-11 15:23:58 +0100

    remove unused vars

--- rickandmorty/__init__.py
+++ rickandmorty/__init__.py
@@ -1,8 +1,5 @@
 """Top-level package for rickandmorty."""
 
-__author__ = """Charlie B"""
-__email__ = "charlieb"
-__version__ = "0.1.0"
 
 from .characters import Characters
 from .episodes import Episodes
